01abe12972db5624cce268348c2e193375175ca1
PiyushChaudhari <piyu181203@gmail.com>
2014-07-05 09:08:33 +0530

    First Commit

---
+++ grails-app/services/com/service/AutoComplateService.groovy
@@ -0,0 +1,11 @@
+package com.service
+
+import com.grails.Domain.AutoComplate
+
+class AutoComplateService {
+
+    def getAutoComplateName(params){
+		def names = AutoComplate.withCriteria {ilike 'name', params.term + '%'}
+		return names
+	}
+}
---
+++ test/unit/com/service/AutoComplateServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.service
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(AutoComplateService)
+class AutoComplateServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
